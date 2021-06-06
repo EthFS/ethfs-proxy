571c4bdbbd9916db39202a5bac5a85d2bc4c3763
Hector Chu <hectorchu@gmail.com>
2021-06-06 15:52:05 +0100

    Improve robustness

--- index.js
+++ index.js
@@ -4,20 +4,24 @@ const {utf8ToHex} = require('web3-utils')
 
 Contract.setProvider('https://api.harmony.one')
 
-const server = http.createServer((req, res) => {
-  const sp = req.url.split('/')
-  const address = sp[1]
-  if (!address.startsWith('0x')) return res.end()
-  let path = sp.slice(2).join('/')
-  if (path === '') path = '/'
-
-  const kernel = new Contract(require('./ethfs.abi'), address)
-  kernel.methods.stat(utf8ToHex(path)).call()
-  .then(({fileType}) => {
-    if (fileType == 2) path += '/index.html'
-    return kernel.methods.readPath(utf8ToHex(path), '0x').call()
-  })
-  .then(data => res.end(Buffer.from(data.slice(2), 'hex')))
-})
-
-server.listen(3000, 'localhost')
+http.createServer(async (req, res) => {
+  try {
+    let index = req.url.indexOf('/', 1)
+    if (index < 0) index = req.url.length
+    const address = req.url.slice(1, index)
+    let path = req.url.slice(index)
+    const kernel = new Contract(require('./ethfs.abi'), address)
+    const {fileType} = await kernel.methods.stat(utf8ToHex(path || '/')).call()
+    if (fileType == 2) {
+      if (path[path.length-1] !== '/') {
+        return res.writeHead(301, {Location: req.url+'/'}).end()
+      } else {
+        path += 'index.html'
+      }
+    }
+    const data = await kernel.methods.readPath(utf8ToHex(path), '0x').call()
+    res.end(Buffer.from(data.slice(2), 'hex'))
+  } catch (e) {
+    res.writeHead(404).end()
+  }
+}).listen(3000, 'localhost')
